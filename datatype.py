1c0ecd50ffb1520ae728a6b0450c8b1cc4d6a2fd
LadyVargas64 <ladyvargas64@gmail.com>
2023-03-11 18:23:09 -0500

    Tipos de datos

---
+++ datatype.py
@@ -0,0 +1,35 @@
+# Strings
+print("Hello world")
+print('Hello World')
+print('''Hello world''')
+print("""Hello world""")
+
+# Contatenado informacion con +
+print("Bye" + "World")
+
+# Saber el tipo de dato con type()
+print(type("Funcion type para saber que tipo de datos es"))
+
+#Numbers
+print(type(45))
+print(type(45.1))
+
+# Boolean
+True
+False
+
+# List
+[10,20,30,40]
+["Hello", "Hi", "Morning"]
+[10, "Hello", True, 10.55]
+print(type([10,20,30,40]))
+[]
+
+# Tuples 
+# Las tuplas no se pueden cambiar son inmutables
+(10, 20, 30, 40)
+()
+print(type((1, 10)))
+
+# Dictionaries - Agrupar distintos tipos de datos con un nombre clave
+print(type({"name":"Ryan","lastname":"Ray", "nickname":"Rayito"}))
