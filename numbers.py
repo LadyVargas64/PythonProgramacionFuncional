9c760b62fb83795861d0cda36496e53d435f0396
LadyVargas64 <ladyvargas64@gmail.com>
2023-03-12 16:28:15 -0500

    Numbers

---
+++ numbers.py
@@ -0,0 +1,17 @@
+10
+14.4
+
+print(type(9))
+print(type(10.2))
+
+print(1 + 1)
+
+# Decir por consola un dato
+age = input("Ingrese edad: ")
+
+# Convertir lo ingresado a un numero
+newAge = int(age) + 5
+
+# Convetir un numero en un string
+print("La edad ingresada es de: " + str(newAge))
+
