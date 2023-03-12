3e52875be6acd559bc9d71cf4890004a2bbba200
LadyVargas64 <ladyvargas64@gmail.com>
2023-03-12 11:12:58 -0500

    Variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+# Variables
+name = "Lady"
+print(name)
+
+x = 100
+
+# Case sensitive
+book = "Python a fondo"
+Book = "Javascript"
+
+x2, book2 = 200, "React"
+print(x2)
+print(book2) 
+
+# Convenciones
+book_name = "La bibli de los caidos" # Snake Case
+bookName = "Frankenstein" # Camel Case
+BookName = "Dracula" # Pascal Case
+
+# Constante
+PI = 3.1416
+MYNAME = "Yohana"
+print(MYNAME)
