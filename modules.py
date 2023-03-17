3cbaded9d617660de656dfeb7221599916a15d12
LadyVargas64 <ladyvargas64@gmail.com>
2023-03-17 11:26:50 -0500

    Moduls

---
+++ modules.py
@@ -0,0 +1,8 @@
+import datetime
+import myMath
+
+print(datetime.date.today())
+print(datetime.timedelta(minutes=70))
+
+myMath.add(1,2)
+myMath.substract(1,2)
---
+++ myMath.py
@@ -0,0 +1,6 @@
+def add(num1, num2):
+    print(num1+num2)
+
+def substract(num1, num2):
+    print(num1-num2)
+
