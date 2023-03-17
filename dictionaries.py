aebcab6b4e5958e28f67a7aa2c079c0f57eec28d
LadyVargas64 <ladyvargas64@gmail.com>
2023-03-17 09:54:13 -0500

    Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,31 @@
+product = {
+    "name": "Book",
+    "quality": 3,
+    "price": 4.99
+}
+
+person = {
+    "firstName": "Ryan",
+    "lastName": "Ray"
+}
+
+# Obtener las llaves
+print(person.keys())
+
+# Obtener los items
+print(person.items())
+
+products = [
+    {
+        "name": "Book",
+        "quality": 3,
+        "price": 4.99
+    },
+    {
+        "name": "Book",
+        "quality": 3,
+        "price": 4.99
+    }
+]
+
+print(products)
